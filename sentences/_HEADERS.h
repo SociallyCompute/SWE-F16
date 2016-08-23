2c32a012442f86524d47b07c692749c3d88fcf0a
wenqiang bo <wb334@mail.missouri.edu>
2016-08-23 16:47:10 -0500

    create new story by wb334

--- sentences/_HEADERS.h
+++ sentences/_HEADERS.h
@@ -5,6 +5,7 @@ int bensSentence_FINAL();
 //
 // DECLARE YOUR symbols here, implement them in a standalone C file
 // vvvvvvvvvvvvvvv  Student work goes down here
-
+extern int wb_MySentence1();
+extern int wb_MySentence2();
 
 
