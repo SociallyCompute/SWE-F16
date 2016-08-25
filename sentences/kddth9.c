aabd1cf009e93b90c00a5afcd0f5b30cf429ac7d
Kienan DeLaney <kddth9@mail.missouri.edu>
2016-08-25 10:40:32 -0500

    Update kddth9.c

--- sentences/kddth9.c
+++ sentences/kddth9.c
@@ -4,12 +4,12 @@
 // Function to include & executeA
 int kddth9Sentence1()
 {
-  printf("They don't carry my size\n");
+  printf("Dinosaurs died out 65 million years ago, according to mainstream science.\n");
   return 1;
 }
 
 int kddth9Sentence2()
 {
-  printf("Perhaps I'll try Amazon. \n");
+  printf("According to cryptozoology and certain other branches of fringe science, a few dinosaurs could have survied much longer and could even be around today. \n");
   return 1;
 }
