97f61fd1562e994515d1c75d88e6c7276b107def
kienandelaney <kddth9@mail.missouri.edu>
2016-08-25 08:15:49 -0500

    Updated Sentences

--- sentences/_HEADERS.h
+++ sentences/_HEADERS.h
@@ -6,5 +6,5 @@ int bensSentence_FINAL();
 // DECLARE YOUR symbols here, implement them in a standalone C file
 // vvvvvvvvvvvvvvv  Student work goes down here
 
-
-
+int kddth9Sentence1();
+int kddth9Sentence2();
---
+++ sentences/kddth9.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+
+// Function to include & executeA
+int kddth9Sentence1()
+{
+  printf("They don't carry my size\n");
+  return 1;
+}
+
+int kddth9Sentence2()
+{
+  printf("Perhaps I'll try Amazon. \n");
+  return 1;
+}
