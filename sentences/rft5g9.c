2fa7a290cbeae4fc494f9c04f09d4677b5acc236
Ryan Tray <rft5g9@mail.missouri.edu>
2016-08-25 11:10:11 -0500

    Adding my two lines to the story

---
+++ sentences/rft5g9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+// Function to include & executeA
+int ryansSentence1()
+{
+	printf("  She also knew that she she was having her friend the Kraken over for dinner and she had nothing to make!  ");
+    	return 1;
+}
+
+int ryansSentence_FINAL()
+{	
+	printf("  Nessie was thankful for the sandwich but knew she would need more food to share with the Kraken.");
+    	return 1;
+}
