4a5d883f78f622e71cb9cc5d4df9a5500faf128a
btomblinson <btomblinson@users.noreply.github.com>
2016-08-24 20:22:15 -0500

    Update _HEADERS.h

--- sentences/_HEADERS.h
+++ sentences/_HEADERS.h
@@ -2,6 +2,8 @@
 // vvvvvvvvvvvvvvv  BASE of exercise
 int bensSentence1();
 int bensSentence_FINAL();
+extern int wb_MySentence1();
+extern int wb_MySentence2();
 void btkvfsentence1();
 void btkvfsentence2();
 //
