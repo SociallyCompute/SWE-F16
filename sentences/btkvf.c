0853d853812d4c70262d44f4a50f6b8f82bb4938
btomblinson <btomblinson@users.noreply.github.com>
2016-08-24 20:19:33 -0500

    Update btkvf.c

--- sentences/btkvf.c
+++ sentences/btkvf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 void btkvfsentence1(){
-	printf("A wizard appeared before the great Linus Torvald\n");
+	printf(No said bigfoot\n");
 }
 
 void btkvfsentence2(){
-	printf("The wizard explained that they were in need of a system to control his apprentice's spell collection which was always being overwritten and broken by fellow wizards\n");
+	printf("But I have heard of amazon he replied\n");
 }
