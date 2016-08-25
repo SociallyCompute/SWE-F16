3b57226ab792fb000e00abdff993fcec141bde58
Christopher King <cakd47@mail.missouri.edu>
2016-08-25 17:48:31 -0500

    my sentences

--- sentences/_HEADERS.h
+++ sentences/_HEADERS.h
@@ -5,6 +5,9 @@ int bensSentence_FINAL();
 //
 // DECLARE YOUR symbols here, implement them in a standalone C file
 // vvvvvvvvvvvvvvv  Student work goes down here
+int cakd47Sentence1();
+int cakd47Sentence2();
+
 int KurtsSentence1();
 int KurtsSentence_FINAL();
 
