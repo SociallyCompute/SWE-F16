3690e392bb4a85d88566e74276e8b0e15646c9f0
btomblinson <btomblinson@users.noreply.github.com>
2016-08-24 20:28:17 -0500

    Update btkvf.c

--- sentences/btkvf.c
+++ sentences/btkvf.c
@@ -2,8 +2,10 @@
 
 void btkvfsentence1(){
 	printf(No said bigfoot\n");
+	printf("But I have heard of amazon he replied\n");
 }
 
 void btkvfsentence2(){
-	printf("But I have heard of amazon he replied\n");
+	printf("It is pretty much the same thing\n");
+	printf("Here I will show you\n");
 }
