292e11b74e292a4322ec7b9c4b12dc2e7ee12c6b
Kienan DeLaney <kddth9@mail.missouri.edu>
2016-08-25 12:53:43 -0500

    Update kddth9.c

--- sentences/kddth9.c
+++ sentences/kddth9.c
@@ -1,18 +1,5 @@
 <<<<<<< HEAD
-# include <stdio.h>
 
-
-// Function to include & executeA
-int kddth9Sentence1()
-{
-  printf("Dinosaurs died out 65 million years ago, according to mainstream science.\n");
-  return 1;
-}
-
-int kddth9Sentence2()
-{
-  printf("According to cryptozoology and certain other branches of fringe science, a few dinosaurs could have survied much longer and could even be around today. \n");
-=======
 #include <stdio.h>
 
 int kienanSentence1(){
