9551d30d8ef951a6d540e03eb30fbaacb94b303b
wenqiang bo <wb334@mail.missouri.edu>
2016-08-25 17:47:40 -0500

    update

---
+++ sentences/mjrqq3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int matthewsSentence1(){
+            printf("there was a link, a missing link that is, called 404.\n");
+                    return 1;
+}
+
+int matthewsSentence2(){
+            printf("\nHe just wanted to tell you how he was feeling, had to make you understand, that he was never going to give you up or let you down.\n");
+                    return 1;
+}
+
