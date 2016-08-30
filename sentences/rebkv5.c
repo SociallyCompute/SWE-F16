3230fe4b12ddef5c97d65655875751671429f50c
rebkv5 <rebkv5@mail.missouri.edu>
2016-08-29 21:30:19 -0500

    Add files via upload
    Sentences for the story

---
+++ sentences/rebkv5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int rebkv5Sentence1(){
+	printf("Regardless of modern science or some convoluted reason, dinosaurs existed in this very plane of existance.\n");
+return 1;
+}
+
+int rebkv5Sentence2(){
+	printf("The war was violent and bloody. Many of the ninjas, samurai, and dinosaurs didn't survive the attack and a great funeral was held.\n");
+return 1;
+}
