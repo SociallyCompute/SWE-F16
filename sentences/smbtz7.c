ccb90ed7696fe0d1602cdebbf58886c8bacb1984
smbtz7 <smbtz7@mail.missouri.edu>
2016-09-05 20:30:43 -0500

    Create smbtz7.c

---
+++ sentences/smbtz7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Sydney Bates Sentence Code
+
+int sydSentence1(){
+	printf("The Ninjas then went to the store to buy more RAM.");
+	return 1;
+}
+
+int sydSentence2(){
+	printf("But they also like to eat pepporoni pizza");
+	return 1;
+}
